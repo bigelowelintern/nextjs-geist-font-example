7c660079705ab84469964f0d375a0e5ea8f7319b
bigelowelintern <eli.bigelow@icloud.com>
2025-07-25 15:04:28 -0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753481068213

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e29303bd._.js",
+  "static/chunks/src_b08deb0b._.js"
+],
+    source: "dynamic"
+});
